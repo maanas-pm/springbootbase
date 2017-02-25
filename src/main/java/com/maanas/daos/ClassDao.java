30b09826c6dcd127450f74172dbb7c3970b20c73
maanas-pm <maanas.pm@gmail.com>
2017-02-25 19:39:19 +0530

    new files added

--- src/main/java/com/maanas/daos/ClassDao.java
+++ src/main/java/com/maanas/daos/ClassDao.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.maanas.models.User;
+import com.maanas.models.Class;
 
 @Transactional
-public interface UserDao extends JpaRepository<User, Long> {
+public interface ClassDao extends JpaRepository<Class, Long> {
 	
-	public List<User> findByEmail(String email);
+	public List<Class> findByName(String name);
 }
